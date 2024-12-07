299cd42d679db1123812630dba7b1f8429683f39
Rodrigo Quadros <rodrigorquadros@gmail.com>
2024-12-07 13:16:17 -0300

    map another elements in the login page

--- tests/pages/loginPage.js
+++ tests/pages/loginPage.js
@@ -1,8 +1,16 @@
 const { waitForElementVisible } = require('../utils/helpers');
 
 class LoginPage {
+    get signupButton() {
+        return $('android=new UiSelector().text("Create an account")')
+    }
+
     get phoneOptionButton() {
         return $('android=new UiSelector().text("Phone")');
+    }
+
+    get emailOptionButton() {
+        return $('android=new UiSelector().text("Email")');
     }
 
     get phoneInput() {
@@ -12,16 +20,32 @@ class LoginPage {
     get continueButton() {
         return $('android=new UiSelector().text("Continue")');
     }
+
+    async signUpButton() {
+        await waitForElementVisible(this.signupButton);
+        await this.signUpButton.click();
+    }
     
     async selectPhoneOption() {
         await waitForElementVisible(this.phoneOptionButton);
         await this.phoneOptionButton.click();
+    }
+
+    async selectEmailOption() {
+        const waitForElementVisible(this.emailOptionButton);
+        await this.emailOptionButton.click();
     }
     
     async enterPhoneNumber(phoneNumber) {
         const phoneInputElement = await this.phoneInput;
         await waitForElementVisible(phoneInputElement);
         await phoneInputElement.setValue(phoneNumber);
+    }
+
+    async enterEmailAddress(email) {
+        const emailInputElement = await this.email;
+        await waitForElementVisible(emailInputElement);
+        await emailInputElement.setValue(email);
     }
     
     async clickContinueButton() {
